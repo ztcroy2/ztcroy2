c7ba6a0fbf18e6bd77f3c210ecb019f456030566
ztcroy2 <77685496+ztcroy2@users.noreply.github.com>
2021-01-19 11:18:25 -0500

    Update canvas-script.js

--- canvas-script.js
+++ canvas-script.js
@@ -1,6 +1,9 @@
 window.onload = function() {
 
   var canvas = document.getElementById('flappy-monster-game');
+// Game Object
+  var flappyMonster = new FlappyMonster(canvas);
+  flappyMonster.start();
 
 
 };
