854888525055abd6a757da99db22954e7e09254a
ztcroy2 <77685496+ztcroy2@users.noreply.github.com>
2021-01-25 10:44:57 -0500

    Update wall.js

--- wall.js
+++ wall.js
@@ -1,1 +1,17 @@
+function Wall(canvas) {
+  // Base
+  var wall = this;
+
+  // Global Attributes
+  wall.canvas = canvas;
+  wall.context = wall.canvas.getContext('2d');
+
+  // Specifications
+  wall.x = canvas.width;
+  wall.y = 0;
+  wall.w = 100;
+  wall.h = 0;
+  wall.gap = 0;
+  wall.color = getRandomColor();
+}
 
